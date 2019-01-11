0865613f5f8ce0464cc01b50663987ea7458104b
CuteCubed <android11089@gmail.com>
2019-01-11 22:14:15 +0100

    Implemented Creation System

--- assets/login/login.js
+++ assets/login/login.js
@@ -18,31 +18,36 @@ $( ".button_choosefile" ).click( function () {
     properties: [ 'openFile' ]
   }, function ( files ) {
     if ( files !== undefined ) {
-      const fs = require( "fs" );
-      var data = fs.readFileSync( files[ 0 ] ) + "";
-      $( $( ".form-row" )[ 1 ] ).hide();
-      $( ".button_choosefile" ).hide();
-      $( ".button_signup" ).hide();
-      $( $( ".form-row .input-group label" ) ).text( "Password" );
-      file = true;
-      $( ".button" ).click( function () {
-        try {
-          keypair = aro.decodeKeypair( data, $( ".field1" ).val() );
-          store.set( 'publickey', keypair.publicCoin );
-          store.set( 'privatekey', CryptoJS.AES.encrypt( keypair.privateCoin, $( ".field1" ).val() ) + "" );
-          location.replace( "./index.html" );
-        } catch ( e ) {
-          $( $( ".form-row .input-group label" ) ).css( "color", "red" );
-          $( ".field1" ).val( "" );
-          setTimeout( function () {
-            $( $( ".form-row .input-group label" ) ).css( "color", "" );
-          }, 1200 );
-          console.log( e );
-        }
-      } );
+      requestPasswordWithFile( files[ 0 ] );
     }
   } );
 } );
+
+function requestPasswordWithFile( path ) {
+  const fs = require( "fs" );
+  var data = fs.readFileSync( path ) + "";
+  $( $( ".form-row" )[ 1 ] ).hide();
+  $( ".button_choosefile" ).hide();
+  $( ".button_signup" ).hide();
+  $( $( ".form-row .input-group label" ) ).text( "Password" );
+  file = true;
+  $( ".button" ).click( function () {
+    try {
+      keypair = aro.decodeKeypair( data, $( ".field1" ).val() );
+      store.set( 'publickey', keypair.publicCoin );
+      store.set( 'privatekey', CryptoJS.AES.encrypt( keypair.privateCoin, $( ".field1" ).val() ) + "" );
+      location.replace( "./index.html" );
+    } catch ( e ) {
+      $( $( ".form-row .input-group label" ) ).css( "color", "red" );
+      $( ".field1" ).val( "" );
+      setTimeout( function () {
+        $( $( ".form-row .input-group label" ) ).css( "color", "" );
+      }, 1200 );
+      console.log( e );
+    }
+  } );
+}
+
 var signup = false;
 $( ".button_signup" ).click( function () {
   if ( !signup ) {
@@ -51,7 +56,9 @@ $( ".button_signup" ).click( function () {
     $( ".button" ).text( "Create" );
     $( $( ".form-row .input-group label" )[ 0 ] ).text( "Password" );
     $( $( ".form-row .input-group label" )[ 1 ] ).text( "Repeat Password" );
+    $( ".button_choosefile" ).hide();
   } else {
+    $( ".button_choosefile" ).show();
     $( "h1" ).text( "Login" );
     $( ".button_signup" ).text( "Create" );
     $( ".button" ).text( "Login" );
@@ -77,12 +84,36 @@ function createWallet() {
   var password = $( ".field1" ).val();
   var password2 = $( ".field2" ).val();
 
-  if ( password == password2 ) {
+  if ( password == password2 && password != "" && password.length > 4 ) {
+    var keypair = aro.encodeKeypair();
+
+    $( $( ".form-row .input-group label" )[ 0 ] ).text( "PublicKey" );
+    $( $( ".form-row .input-group label" )[ 1 ] ).text( "PrivateKey" );
+    $( ".field1" ).val( keypair.publicCoin );
+    $( ".field2" ).val( keypair.privateCoin );
+    $( ".button_signup" ).hide();
+    $( ".button" ).text( "Login" );
+    file = true;
+
+    const path = require( 'path' );
+    const userDataPath = ( electron.app || electron.remote.app ).getPath( 'userData' );
+    var paths = path.join( userDataPath, 'wallet.aro' );
+
+    aro.encryptAro( keypair.encoded, password ).then( encrypt => {
+      var fs = require( 'fs' );
+      fs.writeFileSync( paths, encrypt, 'utf-8' );
+    } );
+
+    console.log( paths );
+
+    $( ".button" ).click( function () {
+      requestPasswordWithFile( paths );
+    } );
 
   } else {
     $( $( ".form-row .input-group label" ) ).css( "color", "red" );
     $( ".field1" ).val( "" );
-    $( ".field2" ).val( "" );
+    $( ".field2 " ).val( "" );
     setTimeout( function () {
       $( $( ".form-row .input-group label" ) ).css( "color", "" );
     }, 1200 );

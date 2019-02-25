f5e247dcea170918c91bb7fe433e1afab852becf
CuteCubed <android11089@gmail.com>
2019-02-25 23:01:44 +0100

    Typos!

--- site/information/js/index.js
+++ site/information/js/index.js
@@ -1,84 +1,84 @@
-var qrcode = new QRCode( "qrcode", {
+var qrcode = new QRCode("qrcode", {
   text: "eXampleCode0==))()(/(/&&))",
   width: 128,
   height: 128,
   colorDark: "#292929",
   colorLight: "#ffffff",
   correctLevel: QRCode.CorrectLevel.H
-} );
-$( "#qrcode" ).prop( 'title', '' );
+});
+$("#qrcode").prop('title', '');
 
 
-function updateQRCode( text, size ) {
-  $( "#qrcode" ).empty();
-  var qrcode1 = new QRCode( "qrcode", {
+function updateQRCode(text, size) {
+  $("#qrcode").empty();
+  var qrcode1 = new QRCode("qrcode", {
     text: "text",
     width: size,
     height: size,
     colorDark: "#292929",
     colorLight: "#ffffff",
     correctLevel: QRCode.CorrectLevel.H
-  } );
-  $( "#qrcode" ).prop( 'title', '' );
-  qrcode1.makeCode( text );
+  });
+  $("#qrcode").prop('title', '');
+  qrcode1.makeCode(text);
 }
 
 
 
-$( ".address" ).val( address );
-$( ".publickey" ).val( publickey );
-$( ".privatekey" ).val( privatekey );
+$(".address").val(address);
+$(".publickey").val(publickey);
+$(".privatekey").val(privatekey);
 
 
-$( ".mobileQRbutton" ).click( function () {
-  showModal( "Show your PrivateKey",
+$(".mobileQRbutton").click(function() {
+  showModal("Show your PrivateKey",
+    "Your PrivateKey should <b style='color:red'>never</b> get into any other persons hand! <b>Refrain from sharing this key!</b> " + "<br>" + "<br>" +
+    "<i>Enter your Wallet-Password</i>",
+    "password",
+    function(password) {
+      var decrypted;
+      try {
+        decrypted = CryptoJS.AES.decrypt(privatekey, password);
+        updateQRCode(address + "|" + decrypted.toString(CryptoJS.enc.Utf8) + "|" + publickey, 190);
+        $(".mobileQRbutton").hide();
+        $("#qrcode").removeClass("blurred");
+      } catch (e) {
+        console.log(e);
+        $($(".form-row .input-group label")).css("color", "red");
+        setTimeout(function() {
+          $($(".form-row .input-group label")).css("color", "");
+        }, 1200);
+      }
+    }, false, true);
+});
+
+$(".privatekeybutton").click(function() {
+  showModal("Show your PrivateKey",
     "Your PrivateKey should <b style='color:red'>never</b> get into any other persons hand! <b>Refain from sharing this key!</b> " + "<br>" + "<br>" +
     "<i>Enter your Wallet-Password</i>",
     "password",
-    function ( password ) {
+    function(password) {
       var decrypted;
       try {
-        decrypted = CryptoJS.AES.decrypt( privatekey, password );
-        updateQRCode( address + "|" + decrypted.toString( CryptoJS.enc.Utf8 ) + "|" + publickey, 190 );
-        $( ".mobileQRbutton" ).hide();
-        $( "#qrcode" ).removeClass( "blurred" );
-      } catch ( e ) {
-        console.log( e );
-        $( $( ".form-row .input-group label" ) ).css( "color", "red" );
-        setTimeout( function () {
-          $( $( ".form-row .input-group label" ) ).css( "color", "" );
-        }, 1200 );
+        decrypted = CryptoJS.AES.decrypt(privatekey, password);
+        $(".privatekey").val(decrypted.toString(CryptoJS.enc.Utf8));
+        $(".privatekeybutton").hide();
+        $(".privatekey").removeClass("blurred");
+      } catch (e) {
+        console.log(e);
+        $($(".form-row .input-group label")).css("color", "red");
+        setTimeout(function() {
+          $($(".form-row .input-group label")).css("color", "");
+        }, 1200);
       }
-    }, false, true );
-} );
-
-$( ".privatekeybutton" ).click( function () {
-  showModal( "Show your PrivateKey",
-    "Your PrivateKey should <b style='color:red'>never</b> get into any other persons hand! <b>Refain from sharing this key!</b> " + "<br>" + "<br>" +
-    "<i>Enter your Wallet-Password</i>",
-    "password",
-    function ( password ) {
-      var decrypted;
-      try {
-        decrypted = CryptoJS.AES.decrypt( privatekey, password );
-        $( ".privatekey" ).val( decrypted.toString( CryptoJS.enc.Utf8 ) );
-        $( ".privatekeybutton" ).hide();
-        $( ".privatekey" ).removeClass( "blurred" );
-      } catch ( e ) {
-        console.log( e );
-        $( $( ".form-row .input-group label" ) ).css( "color", "red" );
-        setTimeout( function () {
-          $( $( ".form-row .input-group label" ) ).css( "color", "" );
-        }, 1200 );
-      }
-    }, false, true );
-} );
+    }, false, true);
+});
 
 
-$( ".logout" ).click( function () {
-  store.set( "publickey", "" );
-  store.set( "privatekey", "" );
+$(".logout").click(function() {
+  store.set("publickey", "");
+  store.set("privatekey", "");
 
 
-  location.replace( "./login.html" );
-} );
+  location.replace("./login.html");
+});
